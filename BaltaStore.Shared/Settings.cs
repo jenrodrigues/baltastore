849b41e42b5011915eb6bb6eaeabfab11c221c58
Jenifer Rodrigues <jen.rodrigues@gmail.com>
2021-04-13 18:32:08 +0100

    Atualizando a senha do bd de Desenvolvimento

--- BaltaStore.Shared/Settings.cs
+++ BaltaStore.Shared/Settings.cs
@@ -1,6 +1,6 @@
 namespace BaltaStore.Shared{
     public static class Settings{
-        public static string ConnectionString = @"Server=localhost;Database=BaltaIO;User Id=sa;Password=jp23mr05;";
+        public static string ConnectionString = @"Server=localhost;Database=BaltaIO;User Id=sa;Password=senhabddev;";
     }
 }
 
